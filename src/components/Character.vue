f965357c3c462e3e3c6d2836fc74e39048a3bb85
Brian Hague <bhague@LT-C02F6AJMMD6N.lan>
2021-06-16 21:47:09 -0500

    added character features down to episodes

---
+++ src/components/Character.vue
@@ -0,0 +1,51 @@
+<template>
+    <article>
+        <h2>{{character.name}}</h2>
+        <img :src="`${character.image}`" :alt="`${character.name}`">
+        <p>{{character.name}} is a {{character.gender}} {{character.species}}</p>
+        <p>Who's status is {{character.status}}</p>
+        <p>{{character.name}} is originally from {{character.origin.name}} and is currently in {{character.location.name}}</p>
+        <h3>Episodes {{character.name}} is in:</h3>
+        <CharacterEpisode v-bind:episode="episode" v-for="episode in episodes" v-bind:key="episode.id" />
+    </article>
+</template>
+
+<script>
+import axios from 'axios'
+import CharacterEpisode from './CharacterEpisode.vue'
+
+export default {
+    name: "Character",
+    props: ["character"],
+    data() {
+        return {
+            episodes: []
+        }
+    },
+    components: {
+        CharacterEpisode
+    },
+    created() {
+        this.character.episode.forEach(episode => {
+            axios.get(episode)
+             .then(({data}) => {
+                 //pasrse the seasons and episodes, then add episodes to local state
+                 const [season, episode] = data.episode.split("S")[1].split("E")
+                 this.episodes = [...this.episodes, {
+                    name: data.name,
+                    air_date: data.air_date,
+                    season, 
+                    episode,
+                    url: data.url,
+                    id: data.id
+                }]
+             })
+        })
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/CharacterEpisode.vue
@@ -0,0 +1,17 @@
+<template>
+    <aside>
+        <h3>Season {{episode.season}} episode {{episode.episode}}:{{episode.name}}</h3>
+        <p>OAir Date: {{episode.air_date}}</p>
+    </aside>
+</template>
+
+<script>
+export default {
+    name: "CharacterEpisode",
+    props: ["episode"]
+}
+</script>
+
+<style scoped>
+
+</style>
