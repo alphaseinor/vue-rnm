72c4fecbcd75da384e10fd3a55a41fe2c2740cfa
Brian Hague <bhague@LT-C02F6AJMMD6N.lan>
2021-06-16 22:49:50 -0500

    added pagination with oice statement

---
+++ src/components/Paginate.vue
@@ -0,0 +1,17 @@
+/*
+    Outputs
+        component containing buttons or other links that emit page-number 
+
+    Inputs (props)
+        required: currentPage, numberOfPages
+        currentPage is the page number that the user is on, this will return a class of "disabled"
+        numberOfPages is 1 to number of pages inclusive. 
+
+    Constraints
+        returns a div with anchor tags, clicking the anchor tag will emit that anchor tag number. 
+
+    Edge cases
+        1 and last page
+        returns page 1 if request is lower than 1
+        last page if request is higher
+ */
